ec1decd79c73284f15766cf71a158ba642a19150
itsAki02 <akanksha021817@gmail.com>
2023-11-03 19:20:07 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,68 @@
+// turn pages when click next or prev button
+
+const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
+
+pageTurnBtn.forEach((el,index) => {
+    el.onclick = () => {
+        const pageTurnId = el.getAttribute('data-page');
+        const pageTurn = document.getElementById(pageTurnId);
+
+        if(pageTurn.classList.contains('turn')) {
+            pageTurn.classList.remove('turn');
+            setTimeout(() => {
+                pageTurn.style.zIndex = 20 - index;
+            },500);
+        }
+        else{
+            pageTurn.classList.add('turn');
+            setTimeout(() => {
+                pageTurn.style.zIndex = 20 + index;
+            },500);
+        }
+    }
+})
+
+// contact me button when click
+
+const pages = document.querySelectorAll('.book-page.page-right');
+const contactMeBtn = document.querySelector('.btn.contact');
+
+contactMeBtn.onclick = () => {
+    pages.forEach((page,index) => {
+        setTimeout(() => {
+            page.classList.add('turn');
+
+            setTimeout(() => {
+                pages.style.zIndex = 20 + index;
+            }, 500);
+        },(index + 1) * 200 + 100)
+    })
+}
+
+//create reverse index function
+let totalPages = pages.length;
+let pageNo = 0;
+
+function reverseIndex() {
+    pageNo--;
+    if(pageNo < 0){
+        pageNo = totalPages - 1;
+    }
+}
+
+//back profile button when click
+const backProfileButton = document.querySelector('.back-profile');
+
+backProfileButton.onclick = () => {
+    pages.forEach((_,index) => {
+        setTimeout(() => {
+            reverseIndex();
+            pages[pageNo].classList.remove('turn');
+
+            setTimeout(() => {
+                reverseIndex();
+                pages[pageNo].style.zIndex = 10 + index;
+            })
+        },(index + 1)* 200 + 100)
+    })
+}
